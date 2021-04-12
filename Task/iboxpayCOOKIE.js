01d53f62656f6b7c65b90de0c7aad7372dcc9b07
hjyon5970 <82366008+hjyon5970@users.noreply.github.com>
2021-04-12 15:26:09 +0800

    Add files via upload

---
+++ Task/iboxpayCOOKIE.js
@@ -0,0 +1,84 @@
+
+
+/* ziye
+独立COOKIE文件
+github地址 https://github.com/ziye888
+TG频道地址  https://t.me/ziyescript
+TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
+boxjs链接  https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye.boxjs.json
+
+转载请备注个名字，谢谢
+
+方式一
+在boxjs里选择复制会话 粘贴至cookie1处
+
+
+
+方式三
+把每一个ck在分别``里面填写，多账号换行 
+
+*/
+
+modulees = 3 //1 选择方式一 3 选择方式三
+
+
+
+
+
+//方式一
+let cookie1 = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let refreshtokenVal= `bf27bb43e06b41ceadb9ce09e2e1c9a7
+ce601c97bae0467f9d48e9ab3509a43c
+1ddac2334731427586161144072378a1
+7e31cadded554de6901cb1b1c8b1ab99
+66e8d1c9fa4e4cb5a264822358624010
+ebaa0b3df085447a910fe4c8b1ee5b49
+095733d4ea1840d28c1690f79879667d
+2e016fdd06ab4b379883963b70206224
+7fe23547dd84443e82a7bf11bf33989f
+46ce28a8e9ed4456923ec5fa2973b9e5
+e5428afc5df946da89828c5523b6d5fa
+2c8775e6a0974c57bffaeb09e8af7cd8
+4f73e8ad405a4fcb965da9d98ecbd87e`
+let iboxpayvideoheaderVal= ``
+let iboxpayvideobodyVal= ``
+
+
+let cookie3 = {
+
+  refreshtokenVal: refreshtokenVal, 
+    iboxpayvideoheaderVal: iboxpayvideoheaderVal,  
+      iboxpayvideobodyVal: iboxpayvideobodyVal,   
+  
+}
+
+
+
+if (modulees == 1) {
+    module.exports = cookie1
+}
+
+if (modulees == 2) {
+    module.exports = cookie2
+}
+
+
+if (modulees == 3) {
+    module.exports = cookie3
+}
+  
+
+
